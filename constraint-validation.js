1a95e80e03d0ada0cbe5b08a24971bbc6c0e3b90
Chris Salvador <csalvador58@gmail.com>
2022-12-01 13:58:24 -0800

    Completed constraint validation problem

--- constraint-validation.js
+++ constraint-validation.js
@@ -11,13 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Used 'blur' to run function once a user leaves an input field. The useCapture argument was set to true to allow capture of all 'blur' events
     document.addEventListener('blur', function(e) {
 
-        console.log(e.target)
+        // console.log(e.target)
+        // console.log(e.target.value)
 
-        // Check if form element includes partial id name 'name', then run checkNameError function
-        if(e.target.id.slice(-4) == 'name') checkNameError(e, e.target.value);
+        // Check if form element includes partial id name 'name', then run isNameInvalid function
+        if(e.target.id.slice(-4) == 'name') isNameInvalid(e.target, e.target.value);
 
-        // Check if form id element is 'email', then run checkNameError function
-        if(e.target.id == 'email') checkEmailError(e, e.target.value);
+        // Check if form id element is 'email', then run isNameInvalid function
+        if(e.target.id == 'email') isEmailInvalid(e.target, e.target.value);
 
         // form.reportValidity();
     }, true);
@@ -25,35 +26,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
     form.addEventListener('submit', function(e) {
         e.preventDefault();
-        // console.log(e.target.validity.valid);
-        console.log(firstName)
-        checkNameError(firstName, firstName.value);
-        checkNameError(lastName,lastName.value);
-        checkEmailError(email, email.value);
+        
+        // Run all invalid checks
+        isNameInvalid(firstName, firstName.value)
+        isNameInvalid(lastName,lastName.value)
+        isEmailInvalid(email, email.value)
+
+        // If name and email fields are valid, continue with submit
+        if (! (firstName.validity.customError && lastName.validity.customError && email.validity.customError)) {
+            form.submit();
+        }       
     });
 
 
     // If email does not match requirement, custom validity error is set and invalid class is added until user corrects input 
-    function checkEmailError (e, email) {
+    function isEmailInvalid (e, email) {
         const regex = /\w+@\w+\.\w+/gi;
         if(!regex.test(email)) {
-            e.setCustomValidity('Email is invalid');
             e.classList.add('invalid'); 
+            e.setCustomValidity('Email is invalid');
         } else {
-            e.setCustomValidity('');
             e.classList.remove('invalid'); 
+            e.setCustomValidity('');
         }  
     }
 
     // If first or last name does not match requirement, custom validity error is set and invalid class is added until user corrects input 
-    function checkNameError (e, name) {
-        console.log(e)
+    function isNameInvalid (e, name) {
+        // console.log(`e: ` + e + ` name: ` + name.length)
         if(name.length < 3) {
-            e.setCustomValidity('Name must be at least 3 characters');
             e.classList.add('invalid'); 
+            e.setCustomValidity('Name must be at least 3 characters');
+            
         } else {
-            e.setCustomValidity('');
             e.classList.remove('invalid'); 
+            e.setCustomValidity('');
         }
     }
 
