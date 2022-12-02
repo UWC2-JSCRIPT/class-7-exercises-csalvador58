ab80392aae9ff00d5360e31dd2064513da065714
Chris Salvador <csalvador58@gmail.com>
2022-12-01 18:31:39 -0800

    Added drop down selection feature

--- contact-me.js
+++ contact-me.js
@@ -9,30 +9,50 @@ document.addEventListener('DOMContentLoaded', function() {
     // Used 'blur' to run function once a user leaves an input field. The useCapture argument was set to true to allow capture of all 'blur' events
     document.addEventListener('blur', function(e) {
 
+        console.log(e.target.id)
+
         // Check if form element includes partial id name 'name', then run isNameInvalid function
         if(e.target.id == 'name') isNameInvalid(e.target, e.target.value);
 
         // Check if form id element is 'email', then run isNameInvalid function
         if(e.target.id == 'email') isEmailInvalid(e.target, e.target.value);
 
+        // Check reason for contacting drop down has a valid selection
+        if(e.target.id == 'contact-reason') isReasonInvalid(e.target, e.target.value);
+
         // form.reportValidity();
     }, true);
 
 
-    // form.addEventListener('submit', function(e) {
-    //     e.preventDefault();
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
         
-    //     // Run all invalid checks
-    //     isNameInvalid(firstName, firstName.value);
-    //     isNameInvalid(lastName,lastName.value);
-    //     isEmailInvalid(email, email.value);
+        // Run all invalid checks
+        isNameInvalid(fullName, fullName.value);
+        isEmailInvalid(email, email.value);
 
-    //     // If name and email fields are valid, continue with submit
-    //     if (! (firstName.validity.customError && lastName.validity.customError && email.validity.customError)) {
-    //         form.submit();
-    //     }
-    // });
+        // If name and email fields are valid, continue with submit
+        if (! (fullName.validity.customError && email.validity.customError)) {
+            form.submit();
+        }
+    });
 
+    function displayAdditionalInfo (info) {
+        if(info == 'job') {
+            document.querySelector('.additional-info-job').style.display = 'block';
+            document.querySelector('.additional-info-talk').style.display = 'none';
+        }
+            
+        if(info == 'talk') {
+            document.querySelector('.additional-info-job').style.display = 'none';
+            document.querySelector('.additional-info-talk').style.display = 'block';
+        }
+
+        if(info == '') {
+            document.querySelector('.additional-info-job').style.display = 'none';
+            document.querySelector('.additional-info-talk').style.display = 'none';
+        }
+    }
 
     // If email does not match requirement, custom validity error is set and invalid class is added until user corrects input 
     function isEmailInvalid (e, email) {
@@ -56,6 +76,22 @@ document.addEventListener('DOMContentLoaded', function() {
             e.classList.remove('invalid'); 
             e.setCustomValidity('');
         }
+    }
+
+
+    // If reason is not selected, custom validity error is set and invalid class is added until user corrects input 
+    function isReasonInvalid (e, reason) {
+
+        console.log(e)
+        if(!reason) {
+            e.classList.add('invalid'); 
+            e.setCustomValidity('Please select and option');
+            
+        } else {
+            e.classList.remove('invalid'); 
+            e.setCustomValidity('');
+        }
+        displayAdditionalInfo(reason);
     }
 
 });
