080b5632cdef7e59d650b6fe2222b50dd9e9cac0
Chris Salvador <csalvador58@gmail.com>
2022-12-01 22:13:03 -0800

    Contact Me problem completed, start work on extra credit

--- contact-me.js
+++ contact-me.js
@@ -4,57 +4,62 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const fullName = document.getElementById('name');
     const email = document.getElementById('email');
+    const message = document.getElementById('message');
     const form = document.getElementById('contact-me');
 
-    // Used 'blur' to run function once a user leaves an input field. The useCapture argument was set to true to allow capture of all 'blur' events
+    // Added a 'blur' listener to detect when a user leaves an input field. The useCapture argument was set to true to allow capture of all 'blur' events
     document.addEventListener('blur', function(e) {
 
-        console.log(e.target.id)
-
-        // Check if form element includes partial id name 'name', then run isNameInvalid function
+        // Check element's ID then run invalid function
         if(e.target.id == 'name') isNameInvalid(e.target, e.target.value);
-
-        // Check if form id element is 'email', then run isNameInvalid function
         if(e.target.id == 'email') isEmailInvalid(e.target, e.target.value);
-
-        // Check reason for contacting drop down has a valid selection
-        if(e.target.id == 'contact-reason') isReasonInvalid(e.target, e.target.value);
+        if(e.target.id == 'message') isMessageInvalid(e.target, e.target.value);
 
         // form.reportValidity();
     }, true);
 
+    // Added a 'change' listener event to run function when dropdown selection is changed
+    document.addEventListener('change', function(e) {
+        if(e.target.id == 'contact-reason') isReasonInvalid(e.target, e.target.value);
+    }, true);
 
+     
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         
         // Run all invalid checks
         isNameInvalid(fullName, fullName.value);
         isEmailInvalid(email, email.value);
+        isMessageInvalid(message, message.value);
 
         // If name and email fields are valid, continue with submit
-        if (! (fullName.validity.customError && email.validity.customError)) {
+        if (! (fullName.validity.customError && email.validity.customError && message.validity.customError)) {
             form.submit();
         }
     });
 
+    // Function will hide/display additional info based on option selected in dropdown menu
     function displayAdditionalInfo (info) {
+        const job = document.querySelector('.additional-info-job');
+        const talk = document.querySelector('.additional-info-talk');
+        
         if(info == 'job') {
-            document.querySelector('.additional-info-job').style.display = 'block';
-            document.querySelector('.additional-info-talk').style.display = 'none';
+            job.style.display = 'block';
+            talk.style.display = 'none';
         }
             
         if(info == 'talk') {
-            document.querySelector('.additional-info-job').style.display = 'none';
-            document.querySelector('.additional-info-talk').style.display = 'block';
+            job.style.display = 'none';
+            talk.style.display = 'block';
         }
 
         if(info == '') {
-            document.querySelector('.additional-info-job').style.display = 'none';
-            document.querySelector('.additional-info-talk').style.display = 'none';
+            job.style.display = 'none';
+            talk.style.display = 'none';
         }
     }
 
-    // If email does not match requirement, custom validity error is set and invalid class is added until user corrects input 
+    // Following functions checks form requirements. If form field is invalid, the custom validity error is set and invalid class is added until user corrects input
     function isEmailInvalid (e, email) {
         const regex = /\w+@\w+\.\w+/gi;
         if(!regex.test(email)) {
@@ -66,7 +71,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }  
     }
 
-    // If first or last name does not match requirement, custom validity error is set and invalid class is added until user corrects input 
+    function isMessageInvalid (e, msg) {
+        if(msg.length < 10) {
+            e.classList.add('invalid'); 
+            e.setCustomValidity('Message must be at least 10 characters');
+            
+        } else {
+            e.classList.remove('invalid'); 
+            e.setCustomValidity('');
+        }
+    }
+
     function isNameInvalid (e, name) {
         if(name.length < 3) {
             e.classList.add('invalid'); 
@@ -78,8 +93,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-
-    // If reason is not selected, custom validity error is set and invalid class is added until user corrects input 
     function isReasonInvalid (e, reason) {
 
         console.log(e)
