486646ac67094a7459df93fa78e97d3922234098
Chris Salvador <csalvador58@gmail.com>
2022-12-02 02:17:06 -0800

    Updated contact me problem, fixed invalid logic

--- contact-me.js
+++ contact-me.js
@@ -1,4 +1,3 @@
-// TODO
 // Check that DOM content had all loaded before running code
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -11,10 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const contactReason = document.getElementById('contact-reason');
     const form = document.getElementById('contact-me');
 
-    // const lookup = {
-        
-    // }
-
     // Added a 'blur' listener to detect when a user leaves an input field. The useCapture argument was set to true to allow capture of all 'blur' events
     document.addEventListener('blur', function(e) {
 
@@ -26,17 +21,13 @@ document.addEventListener('DOMContentLoaded', function() {
         // {EXTRA CREDIT}
         if(e.target.id == 'title') isTitleInvalid(e.target, e.target.value);
         if(e.target.id == 'company') isCompanyInvalid(e.target, e.target.value);
-
-        // form.reportValidity();
     }, true);
 
     // Added a 'change' listener event to run function when dropdown selection is changed
     document.addEventListener('change', function(e) {
         if(e.target.id == 'contact-reason') isReasonInvalid(e.target, e.target.value);
-    }, true);
 
-    // {EXTRA CREDIT}
-    document.addEventListener('change', function(e) {
+        // {EXTRA CREDIT}
         if(e.target.id == 'language') isLanguageInvalid(e.target, e.target.value);
     }, true);
 
@@ -52,19 +43,22 @@ document.addEventListener('DOMContentLoaded', function() {
         if (contactReason.value == 'job') {
             isTitleInvalid(title, title.value);
             isCompanyInvalid(company, company.value);
+            form.reportValidity();
 
-            if (! (fullName.validity.customError && email.validity.customError && message.validity.customError && title.validity.customError && company.validity.customError)) {
+            if (! (fullName.validity.customError + email.validity.customError + message.validity.customError + title.validity.customError + company.validity.customError) > 0) {
                 form.submit();
             }
         } else if (contactReason.value == 'talk') {
             isLanguageInvalid(codeLang, codeLang.value);
+            form.reportValidity();
 
-            if (! (fullName.validity.customError && email.validity.customError && message.validity.customError && codeLang.validity.customError)) {
+            if (! (fullName.validity.customError + email.validity.customError + message.validity.customError + codeLang.validity.customError) > 0) {
                 form.submit();
             }
         } else {
             // If name and email fields are valid, continue with submit
-            if (! (fullName.validity.customError && email.validity.customError && message.validity.customError)) {
+            form.reportValidity();
+            if (! (fullName.validity.customError + email.validity.customError + message.validity.customError) > 0) {
                 form.submit();
             }
         }
@@ -72,31 +66,58 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Function will hide/display additional info based on option selected in dropdown menu
     function displayAdditionalInfo (info) {
+        console.log("log: " + info)
+
         if(info == 'job') {
-            document.querySelector('.additional-info-job').style.display = 'block';
-            document.querySelector('.additional-info-talk').style.display = 'none';
+            // Reset options
             codeLang.value = "";
-            
+            isLanguageInvalid(codeLang, true);
+            isReasonInvalid (contactReason, true);
+            document.querySelector('.additional-info-talk').style.display = 'none';
+
+            // Show Talk options
+            document.querySelector('.additional-info-job').style.display = 'block';
         }
             
         if(info == 'talk') {
-            document.querySelector('.additional-info-job').style.display = 'none';
-            document.querySelector('.additional-info-talk').style.display = 'block';
+            // Reset options
             title.value = "";
             company.value = "";
+            isTitleInvalid(title, true);
+            isCompanyInvalid(company, 'https://wwww.123.com');
+            isReasonInvalid (contactReason, true);
+            document.querySelector('.additional-info-job').style.display = 'none';
+
+            // Show Talk options
+            document.querySelector('.additional-info-talk').style.display = 'block';
         }
 
-        if(info == '') {
+        if(info == "") {
             // Reset to default values
-            document.querySelector('.additional-info-job').style.display = 'none';
-            document.querySelector('.additional-info-talk').style.display = 'none';
             codeLang.value = "";
             title.value = "";
             company.value = "";
+            isLanguageInvalid(codeLang, true);
+            isTitleInvalid(title, true);
+            isCompanyInvalid(company, 'https://wwww.123.com');
+            isReasonInvalid (contactReason, true);
+            document.querySelector('.additional-info-job').style.display = 'none';
+            document.querySelector('.additional-info-talk').style.display = 'none';
         }
     }
 
     // Following functions checks form requirements. If form field is invalid, the custom validity error is set and invalid class is added until user corrects input
+    function isCompanyInvalid (e, company) {
+        const regex = /https?\:\/\/.+\..+/gi;
+        if(!regex.test(company)) {
+            e.classList.add('invalid'); 
+            e.setCustomValidity('URL is invalid');
+        } else {
+            e.classList.remove('invalid'); 
+            e.setCustomValidity('');
+        }
+    }
+
     function isEmailInvalid (e, email) {
         const regex = /\w+@\w+\.\w+/gi;
         if(!regex.test(email)) {
@@ -111,7 +132,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function isLanguageInvalid (e, code) {
         if(!code) {
             e.classList.add('invalid'); 
-            e.setCustomValidity('Please select and option');
+            e.setCustomValidity('Please select an option');
             
         } else {
             e.classList.remove('invalid'); 
@@ -163,16 +184,4 @@ document.addEventListener('DOMContentLoaded', function() {
             e.setCustomValidity('');
         }
     }
-
-    function isCompanyInvalid (e, company) {
-        const regex = /https?\:\/\/.+\..+/gi;
-        if(!regex.test(company)) {
-            e.classList.add('invalid'); 
-            e.setCustomValidity('URL is invalid');
-        } else {
-            e.classList.remove('invalid'); 
-            e.setCustomValidity('');
-        }
-    }
-
 });
